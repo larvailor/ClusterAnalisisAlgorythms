c548492b65e179187a335a5854121297d15472a7
Dima Konovko <dima.konovko@mail.ru>
2020-02-07 16:32:04 +0300

    add: parent class logic to Kmeans algorythm

--- ClusterAnalisisAlgorythms/ClusterAnalisisAlgorythms/Kmeans.hpp
+++ ClusterAnalisisAlgorythms/ClusterAnalisisAlgorythms/Kmeans.hpp
@@ -1,70 +1,22 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <mutex>
+#include "Algorythm.hpp"
 
-struct Point
-{
-	unsigned short x;
-	unsigned short y;
-};
-
-struct Item
-{
-	Point pos;
-
-	Item(unsigned short x, unsigned short y)
-	{
-		pos.x = x;
-		pos.y = y;
-	}
-};
-
-struct Cluster
-{
-	Point kernel;
-	std::vector<Item*> items;
-
-	Cluster(Point pos) : kernel(pos) { }
-};
-
-class Kmeans
+class Kmeans : public Algorythm
 {
 private:
-	/////////////////////////////////////////////////
-	// 
-	//		VARIABLES
-	//
-	/////////////////////////////////////////////////
-
-	unsigned m_nOfItems;
-	unsigned short m_nOfClusters;
-	unsigned short m_areaWidth;
-	unsigned short m_areaHeight;
-
-	std::vector<Item> m_allItems;
-	std::vector<Cluster> m_clusters;
-	std::vector<Cluster> m_clustersCopyForDraw;
-
-	std::mutex m_mutex;
-	bool bSolved;
-
 	/////////////////////////////////////////////////
 	// 
 	//		METHODS
 	//
 	/////////////////////////////////////////////////
 
-	void randomizeItems();
 	void createEmptyClusters(std::vector<Point>& kernels);
 
 	//-----------------------------------------------
 	//		Algorythm
 	//
 
-	void recalculateClusters();
-	double calculateEuclidDistance(Point& point, Point& kernel);
 	void recalculateKernels();
 
 public:
@@ -79,14 +31,6 @@ public:
 	//
 	
 	Kmeans(unsigned nOfItems, unsigned short nOfClusters, std::vector<Point> kernels, unsigned short areaWidth, unsigned short areaHeight);
-
-	//-----------------------------------------------
-	//		Accessors
-	//
-
-		// Getters
-	std::vector<Item>& getAllItems();
-	std::vector<Cluster> getAllClusters();
 
 	//-----------------------------------------------
 	//		Algorythm
